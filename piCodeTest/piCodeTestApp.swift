2e85a42cf60120ca127d647e896bd5204d2587b9
nasirhasanovic <hasanovicnasir@gmail.com>
2025-10-07 18:22:40 +0200

    Initial Commit

---
+++ piCodeTest/piCodeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  piCodeTestApp.swift
+//  piCodeTest
+//
+//  Created by nasir on 7. 10. 2025..
+//
+
+import SwiftUI
+
+@main
+struct piCodeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ piCodeTestTests/piCodeTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  piCodeTestTests.swift
+//  piCodeTestTests
+//
+//  Created by nasir on 7. 10. 2025..
+//
+
+import Testing
+@testable import piCodeTest
+
+struct piCodeTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
